d6b491e2a202d944015edf918d0b4e81e816d6a4
Juan Serna <103015719+NoT-Serna@users.noreply.github.com>
2024-05-16 20:16:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -22,15 +22,10 @@ def run_mpi(rank, size):
     print()
 
 if __name__ == "__main__":
-    num_processes = 10
-    threads = []
-    for i in range(num_processes):
-        thread = threading.Thread(target=run_mpi, args=(i, num_processes))
-        threads.append(thread)
-        thread.start()
-
-    for thread in threads:
-        thread.join()
+      num_processes = 2  # Ajustar
+    ip_list = ['192.168.1.100', '192.168.1.101']  # IPs
+    rank = int(input("Enter the rank of this device (0 or 1): "))
+    run_mpi(rank, num_processes, ip_list)
 
 # Ejecucción en varias máquinas
     '''
