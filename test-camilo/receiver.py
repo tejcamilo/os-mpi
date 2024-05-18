20460e570298d9ee1ea3a3a4b8d00ca07b35cb48
tejcamilo <ctejada@twilio.com>
2024-05-17 20:25:26 -0500

    Implemented funcionality to send and receive files

--- test-camilo/receiver.py
+++ test-camilo/receiver.py
@@ -10,6 +10,23 @@ def receive_message(port):
             data = conn.recv(1024).decode()
             print(f"Receiver received: {data}")
 
+def receive_file(filename, port):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind(('0.0.0.0', port))
+        s.listen(1)
+        print(f"Receiver listening on port {port}...")
+        conn, addr = s.accept()
+        with conn:
+            with open(filename, 'wb') as f:
+                while True:
+                    data = conn.recv(1024)
+                    if not data:
+                        break
+                    f.write(data)
+            print(f"Receiver received: {filename}")
+
 if __name__ == "__main__":
     port = 6042  # Choose the same port used by the sender
-    receive_message(port)
+    #receive_message(port)
+    filename = "received.txt"
+    receive_file(filename, port)
--- test-camilo/sender.py
+++ test-camilo/sender.py
@@ -6,7 +6,7 @@ load_dotenv()  # take environment variables from .env.
 source_ip = os.getenv('SOURCE_IP')
 dest_ip = os.getenv('DESTINATION_IP')
 
-
+print(os.getcwd())
 
 def send_message(message, source_ip, dest_ip, port):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -16,7 +16,27 @@ def send_message(message, source_ip, dest_ip, port):
         s.sendall(message.encode())
         print(f"Sender sent: {message}")
 
+def send_file(filename, source_ip, dest_ip, port):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((source_ip, 0))  # Bind to the source IP address
+        s.connect((dest_ip, port))
+
+        # Open the file in binary mode and send its contents
+        with open(filename, 'rb') as f:
+            while True:
+                data = f.read(1024)
+                if not data:
+                    break
+                s.sendall(data)
+
+        print(f"Sender sent: {filename}")        
+
+#send_file("test.txt", source_ip, dest_ip, 6042)
+
 if __name__ == "__main__":
     port = 6042  # Choose a port to listen on
-    message = "Hello from Sender!"
-    send_message(message, source_ip, dest_ip, port)
+    # message = "Hello from Sender!"
+    # send_message(message, source_ip, dest_ip, port)
+    filename = "/Users/ctejada/Desktop/os-proyecto/os-mpi/test-camilo/test1.py"
+    send_file(filename, source_ip, dest_ip, port)
+    
